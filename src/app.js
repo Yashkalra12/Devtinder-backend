a37b45d9825b1769cef3af27d58aa948d50d02e9
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2024-11-08 20:12:36 +0530

    Server Creation

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log()
+const express = require("express");
+
+const app=express();
+
+app.use("/hello",(req,res)=>{
+    res.send("Hello Yashh here!"); 
+})
+
+app.use("/",(req,res)=>{
+    res.send("Hello from the server!");
+})
+app.listen(3000, ()=>{
+    console.log("server is running on port 3000");
+});
