0e610db27e41466b5eb7e9d1fdc7c412b8a092f5
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2024-11-17 23:24:23 +0530

    Server

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 
 const app=express();
+// app.use("/",(req,res)=>{
+//     res.send("Hello from the server!");
+// });
 
 app.use("/hello",(req,res)=>{
     res.send("Hello Yashh here!"); 
 })
 
-app.use("/",(req,res)=>{
-    res.send("Hello from the server!");
-})
+
 app.listen(3000, ()=>{
     console.log("server is running on port 3000");
 });
